dcc12051c0bfd03712da4a6b8e03217be073a938
caleffa <caleffa@gmail.com>
2025-11-20 09:54:44 -0300

    Cambios en el puerto

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const cors = require('cors');
 const sql = require('mssql');
 const https = require('https');
@@ -18,8 +18,8 @@ app.use(express.json());
 // RUTAS
 app.use('/prc/promo/api', promosRoutes);
 app.use('/api/users', usersRoutes);
-app.use('/api/items/', itemsRoutes);
-app.use('/api/stores/', storesRoutes);
+app.use('/api/items', itemsRoutes);
+app.use('/api/stores', storesRoutes);
 
 // CERTIFICADOS HTTPS
 const options = {
@@ -30,4 +30,30 @@ const options = {
 // Levantar servidor HTTPS
 https.createServer(options, app).listen(process.env.PORT || 9443, () => {
   console.log(`🔐 Servidor HTTPS levantado en puerto ${process.env.PORT || 9443}`);
+});*/
+
+const express = require('express');
+const cors = require('cors');
+//const sql = require('mssql');
+require('dotenv').config();
+
+//const dbconfig     = require('./db/dbconfig');
+const promosRoutes = require('./routes/offers');
+//const usersRoutes  = require('./routes/users');
+//const itemsRoutes  = require('./routes/items');
+//const storesRoutes = require('./routes/stores');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// RUTAS
+app.use('/prc/promo/api', promosRoutes);
+//app.use('/api/users', usersRoutes);
+//app.use('/api/items', itemsRoutes);
+//app.use('/api/stores', storesRoutes);
+
+// Servidor HTTP normal (Render aplica HTTPS por fuera)
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Servidor iniciado en puerto ${process.env.PORT || 3000}`);
 });
